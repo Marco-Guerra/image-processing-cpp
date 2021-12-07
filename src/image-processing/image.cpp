9dd7538c449a604de11640c55535a056c73f1a84
darkvictor13 <01victoralmeida@gmail.com>
2021-12-07 17:45:46 -0300

    Gerando o .cpp e otimizando

---
+++ src/image-processing/image.cpp
@@ -0,0 +1,35 @@
+#include "image.hpp"
+
+Image::Image(const std::string file_path, int flag) {
+	loadImage(file_path, flag);
+}
+
+void Image::loadImage(const std::string file_path, int flag) {
+	mat = cv::imread(file_path, flag);
+}
+
+void Image::medianBlur(int ksize) {
+	cv::medianBlur(mat, mat, ksize);
+}
+
+const cv::Mat& Image::getMat() const {
+	return mat;
+}
+
+wxImage Image::toWxImage() {
+	cv::Mat im2;
+	if (mat.channels() == 1) {
+		cv::cvtColor(mat, im2, cv::COLOR_GRAY2RGB);
+	}
+	else if (mat.channels() == 4) {
+		cv::cvtColor(mat, im2, cv::COLOR_BGRA2RGB);
+	}
+	else {
+		cv::cvtColor(mat, im2, cv::COLOR_BGR2RGB);
+	}
+
+	long imsize = im2.rows * im2.cols * im2.channels();
+	wxImage wx(im2.cols, im2.rows, (unsigned char *)malloc(imsize), false);
+	memcpy(wx.GetData(), im2.data, imsize);
+	return wx;
+}
--- src/image-processing/image.hpp
+++ src/image-processing/image.hpp
@@ -9,43 +9,13 @@ private:
 	cv::Mat mat;
 
 public:
-	Image(const std::string file_path, int flag = 1) {
-		loadImage(file_path, flag);
-	}
+	Image(const std::string file_path, int flag = 1);
+	void loadImage(const std::string file_path, int flag = 1);
 
-	void loadImage(const std::string file_path, int flag = 1) {
-		mat = cv::imread(file_path, flag);
-	}
+	void medianBlur(int ksize);
 
-	void medianBlur(int ksize) {
-		cv::medianBlur(mat, mat, ksize);
-	}
-
-	const cv::Mat &getMat() const {
-		return mat;
-	}
-
-	wxImage toWxImage() {
-		cv::Mat im2;
-		if (mat.channels() == 1) {
-			cv::cvtColor(mat, im2, cv::COLOR_GRAY2RGB);
-		}
-		else if (mat.channels() == 4) {
-			cv::cvtColor(mat, im2, cv::COLOR_BGRA2RGB);
-		}
-		else {
-			cv::cvtColor(mat, im2, cv::COLOR_BGR2RGB);
-		}
-
-		long imsize = im2.rows * im2.cols * im2.channels();
-		wxImage wx(im2.cols, im2.rows, (unsigned char *)malloc(imsize), false);
-		unsigned char *s = im2.data;
-		unsigned char *d = wx.GetData();
-		for (long i = 0; i < imsize; i++) {
-			d[i] = s[i];
-		}
-		return wx;
-	}
+	const cv::Mat &getMat() const;
+	wxImage toWxImage();
 };
 
 #endif // IMAGE
